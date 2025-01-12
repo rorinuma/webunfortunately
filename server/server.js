3e39bb7a5c0ccf0e6b573a0be8fe9fc2eceb1ca0
rorinuma <focusfire2650@gmail.com>
2025-01-12 15:29:37 +0300

    server commit

---
+++ server/server.js
@@ -0,0 +1,114 @@
+const express = require("express")
+const app = express();
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const mysql = require('mysql')
+const jwt = require('jsonwebtoken')
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+}
+require("dotenv").config()
+
+
+app.use(cors(corsOptions));
+app.use(cookieParser())
+app.use(express.json())
+
+var db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("connected");
+})
+
+app.post('/api/register', (req, res) => {
+  const { email, username, password } = req.body
+  const checkQuery = "SELECT * FROM users WHERE username = ? OR email = ?";
+  db.query(checkQuery, [username, email], (checkErr, checkResult) => {
+    if(checkErr) {
+      console.error(checkErr);
+      return res.status(500).json({error: "DB error while checking username"})
+    }
+    if(checkResult.length > 0) {
+      return res.status(400).json({error: "Username already exists"})
+    }
+    const q = "INSERT INTO users (email, username, password) VALUES(?, ?, ?)"
+    db.query(q, [email, username, password], (err, result) => {
+      if(err) {
+        console.error(err)
+        res.status(500).json({error: "DB error"})
+      } else {
+        res.json({message: "user registered successfully"})
+      }
+    })
+  });
+});
+
+
+app.post('/api/login',  (req, res) => {
+  const {username, password} = req.body
+  const checkQuery = "SELECT * FROM users WHERE username = ? AND password = ?";
+  db.query(checkQuery, [username, password], (checkErr, checkResult) => {
+    if(checkErr) {
+      console.error(checkErr);
+      return res.status(500).json({error: "DB error while checking login credentials"})
+    }
+    if(checkResult.length === 1) {
+
+      let jwtSecretKey = process.env.JWT_SECRET_KEY
+      let data = {
+        time: Date(),
+        userId: checkResult[0].id,
+        username: checkResult[0].username
+      }
+      const token = jwt.sign(data, jwtSecretKey, {expiresIn: '1h'})
+      return res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 1000
+      })
+      .status(200)
+      .json({message: 'logged in successfully'})
+    } else {
+      return res.status(401).json({error: "Invalid username or password"})
+    }
+  })
+})
+
+const authorization = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if(!token) {
+    return res.sendStatus(403);
+  }
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.userId = data.userId;
+    req.username = data.username;
+    return next();
+  } catch {
+    return res.sendStatus(403);
+  }
+}
+
+app.get("/api/logout", (req, res) => {
+  return res
+  .clearCookie("access_token")
+  .status(200)
+  .json({message: "successfully logged out"})
+})
+
+app.get("/api/protected", authorization, (req, res) => {
+  return res.json({user: {id: req.userId, username: req.username}})
+})
+
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080")
+});
+
